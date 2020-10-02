26822107906a13e3d84d88af139922c7de2746cc
Lucas Pedersen <lucas.a.pedersen@gmail.com>
2020-10-02 13:20:35 +0200

    done maybe hope fully

--- starships.js
+++ starships.js
@@ -10,14 +10,14 @@ if (url.has("id")) {
         thePlanet.innerHTML = `
         <h2 class="character_name">${data.name}</h2>
         <div class="infoDiv1">
-            <p class="planet_climate">${data.name}'s climate: ${data.climate}</p>
-            <p class="planet_diameter">${data.name}'s diameter: ${data.diameter}km</p>
-            <p class="planet_gravity">${data.name}'s gravity: ${data.gravity}</p>
+            <p class="starship_model">${data.name}'s model: ${data.model}</p>
+            <p class="starship_crew">${data.name}'s crew: ${data.crew}</p>
+            <p class="starship_speed">${data.name}'s max atmosphering speed: ${data.max_atmosphering_speed}</p>
         </div>
         <div class="infoDiv2">
-            <p class="planet_orbital_period">${data.name}'s orbital period: ${data.orbital_period}</p>
-            <p class="planet_population">${data.name}'s population: ${data.population}</p>
-            <p class="planet_rotation_period">${data.name}'s rotation period: ${data.rotation_period}</p>
+            <p class="starship_cost">${data.name}'s cost in credits: ${data.cost_in_credits}</p>
+            <p class="starship_class">${data.name}'s starship class: ${data.starship_class}</p>
+            <p class="starship_manufacturer">${data.name}'s manufacturer: ${data.manufacturer}</p>
         </div>
         `
     });
